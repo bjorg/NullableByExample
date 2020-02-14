a6365a6e6bd7b71188a94c9b58184590430c6d55
Steve G. Bjorg <steve.bjorg@gmail.com>
2020-02-14 14:30:05 -0800

    sample code

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace NullableByExample {
+
+    public class Program {
+
+        //--- Class Methods ---
+        public static void Main(string[] args) {
+
+            // fetch with ID
+            var r1 = Fetch("123");
+            Store(r1);
+
+            // fetch with null
+            var r2 = Fetch(null);
+            if(IsValid(r2)) {
+                var id_r2_2 = r2.Id;
+                Record r2_2 = r2;
+                Store(r2);
+            } else {
+                var id_r2_1 = r2!.Id;
+                Record r2_1 = r2!;
+            }
+
+            // try-fetch success
+            if(TryFetch("123", out var r3)) {
+                Store(r3);
+            }
+
+            // try-fetch failure
+            if(!TryFetch("123", out var r4) && IsValid(r4)) {
+                Store(r4);
+            }
+        }
+
+        public static void Store(Record record) { /* ... */ }
+
+        [return: NotNullIfNotNull("id") ]
+        public static Record? Fetch(string? id) {
+            if(id == null) {
+                return null;
+            }
+            return new Record {
+                Id = id
+            };
+        }
+
+        public static bool TryFetch(string? id, [NotNullWhen(true)] out Record? record) {
+            if(id == null) {
+                record = null;
+                return false;
+            }
+            record = new Record {
+                Id = id
+            };
+            return true;
+        }
+
+
+        public static bool IsValid([NotNullWhen(true)] Record? record) => record != null;
+
+        [return: MaybeNull]
+        public static T FindOrDefault<T>(string? id) => default;
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Nullable by Example
+
+## Getting Started
+
+1. Enable nullable types for the entire project by adding the `<Nullable>enable</Nullable>` element to your `.csproj` files.
+1. Add the `#nullable disable` pragma to every source file in your project.
+1. As you work on each file, remove the pragma and address any warnings.
